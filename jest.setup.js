c1777fd30f6d9374abb0919361bb063266c3c010
Andrea santi <santi_andrea@hotmail.it>
2022-07-29 12:28:48 +0200

    [test HeaderHome]

--- jest.setup.js
+++ jest.setup.js
@@ -3,4 +3,21 @@
 
 // Used for __tests__/testing-library.js
 // Learn more: https://github.com/testing-library/jest-dom
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: jest
+    .fn()
+    .mockImplementation((query) => ({
+      matches:
+        query === "(min-width: 1024px)" ||
+        query === "(max-width: 1024px)",
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // deprecated
+      removeListener: jest.fn(), // deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+});
 import "@testing-library/jest-dom/extend-expect";
